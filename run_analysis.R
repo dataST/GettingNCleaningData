3f90eca1750ed16c86dccbef24fd4291cf067d3f
dataST <one.instr@gmail.com>
2015-02-21 04:45:04 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,10 @@
+## Project description:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 
 
 ## to read raw data:
@@ -11,37 +18,38 @@ yTrain <- read.table("./train/y_train.txt")
 activityLabels <- read.table("./activity_labels.txt")
 features <- read.table("features.txt")
 
-## to name columnes
+## to name columnes before extracting the columns of mean() and std()
 
 rawN <- features$V2
 colnames(xTest) <- rawN
 colnames(xTrain) <- rawN
 
-## extracting mean() and std()
+## to extract the columns of mean() and std()
 
 grepN <- grepl("-(mean|std)\\(\\)", rawN)
 xTest <- xTest[, grepN]
 xTrain <- xTrain[, grepN]
 
-## to merge data:
+## to merge the subject column, the activity column, and extracted mean() and std() columns
 
 trainTbl <- data.frame(cbind(subjectTrain, yTrain, xTrain))
 testTbl <- data.frame(cbind(subjectTest, yTest, xTest))
 mergedData <- merge(testTbl, trainTbl, all=TRUE)
 
-## to use descriptive activity names:
+## to assign descriptive activity names to the column of activities
 
 mergedData[, 2] <- activityLabels[mergedData[, 2], 2]
 
-## Descriptive variable names:
+## To assign descriptive variable names to the columns for subjects and activities
 
 colnames(mergedData)[1] <- "subject"
 colnames(mergedData)[2] <- "activity"
 
-## Second tidy data set with the average of each variable for each activity and each subject:
+## To make the eecond tidy data set with the average of each variable for each activity and each subject:
 
 library(plyr)
 finalData <- ddply(mergedData, .(subject, activity), function(x){colMeans(mergedData[, 3:68])})
 
-## to export the final sata from Step 5:
+## to export the final data set from Step 5:
+
 write.table(finalData, "step5data.txt", row.name=FALSE)
